af8f175a5e243952d4dc744cea4a955157052aad
ES-SOFYANY <abdellah.essofyany@gmail.com>
2017-08-15 11:57:33 +0200

    refactoring

--- App/App.js
+++ App/App.js
@@ -4,42 +4,45 @@ import Position from './models/Position';
 import Mower from './models/Mower';
 
 export default class App {
-  // eslint-disable-next-line
-  startProgrammeAndReturnOutput(input) {
-    const instructions = input.split(/\n/);
-    const lawnSurface = instructions[0].split(' ');
-    const lawn = new Lawn(lawnSurface[0], lawnSurface[1]);
-    let output = '';
-    instructions.forEach((element, index) => {
-      let position;
-      let mower;
-      if (index > 0 && index % 2 === 0) {
-        const positionArgs = instructions[index - 1].split(' ');
-        position = new Position(positionArgs[0], positionArgs[1], positionArgs[2]);
-        mower = new Mower(position, lawn);
-        mower.runInstructions(element);
-        output += `\n ${mower.toString()}`;
-      }
-    });
-    return output;
-  }
-// eslint-disable-next-line 
-  readFile(path) {
-    return new Promise((resolve) => {
-      fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-          throw Error('Error: File can\'t be read');
-        }
-        resolve(data);
-      });
-    });
-  }
+    // eslint-disable-next-line
+    startProgrammeAndReturnOutput(input) {
+        const instructions = input.split(/\n/);
+        const lawnSurface = instructions[0].split(' ');
+        const lawn = new Lawn(lawnSurface[0], lawnSurface[1]);
+        let output = '';
+        instructions.forEach((element, index) => {
+            let position;
+            let mower;
+            if (index > 0 && index % 2 === 0) {
+                const positionArgs = instructions[index - 1].split(' ');
+                position = new Position(positionArgs[0], positionArgs[1], positionArgs[2]);
+                mower = new Mower(position, lawn);
+                mower.runInstructions(element);
+                output += `\n ${mower.toString()}`;
+            }
+        });
+        return output;
+    }
 
-  saveFile(output, path) {
-    fs.writeFile(path, output, (err) => {
-      if (err) {
-        throw Error('Error: file not saved');
-      }
-    });
-  }
+// eslint-disable-next-line
+    readFile(fileInputPath) {
+        return new Promise((resolve) => {
+            fs.readFile(fileInputPath, 'utf8', (err, data) => {
+                if (err) {
+                    throw Error('Error: File can\'t be read');
+                }
+                resolve(data);
+            });
+        });
+    }
+
+    saveOutputInFile(fileOutputPath) {
+        return (output)=> {
+            fs.writeFile(fileOutputPath, output, (err) => {
+                if (err) {
+                    throw Error('Error: file not saved');
+                }
+            });
+        }
+    }
 }
--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import App from './App/App';
 
 const app = new App();
 
-const fileInput = `${__dirname}/Fixtures/exemple.txt`;
-const fileOutput = `${__dirname}/output/output.txt`;
+const fileInputPath = `${__dirname}/Fixtures/exemple.txt`;
+const fileOutputPath = `${__dirname}/output/output.txt`;
 
-app.readFile(fileInput)
+app.readFile(fileInputPath)
     .then(app.startProgrammeAndReturnOutput)
-    .then((outputData)=>{
-        app.saveFile(outputData, fileOutput);
-    });
+    .then(app.saveOutputInFile(fileOutputPath));
 
